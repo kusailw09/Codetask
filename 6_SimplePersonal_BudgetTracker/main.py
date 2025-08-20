940de33bb55ce337811937ab0f40354579a27002
Kusai Latthawala <fromkusai01@gmail.com>
2025-08-21 00:32:26 +0530

    Added Simple Personal Budget Tracker task

---
+++ 6_SimplePersonal_BudgetTracker/main.py
@@ -0,0 +1,9 @@
+income = float(input("Enter your total income: "))
+
+expenses = float(input("Enter your total expenses: "))
+
+balance = income - expenses
+
+print(f"\nIncome: {income}")
+print(f"Expenses: {expenses}")
+print(f"Balance: {balance}")
