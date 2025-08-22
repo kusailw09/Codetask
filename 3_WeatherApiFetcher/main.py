767056f775668f32bc54f82cd3a4d9709b4c146b
Kusai Latthawala <fromkusai01@gmail.com>
2025-08-22 12:21:18 +0530

    Added Weather Api Fetcher

--- 3_WeatherApiFetcher/main.py
+++ 3_WeatherApiFetcher/main.py
@@ -1,5 +1,5 @@
 import requests
 
 city = input("Enter city: ")
-r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY&units=metric").json()
+r = requests.get(f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_REAL_API_KEY&units=metric").json()
 print(f"{city}: {r['weather'][0]['description']}, {r['main']['temp']}°C" if r.get("main") else "City not found!")
